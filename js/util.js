7fe77017a9774bf0de8c29429d71b881997341fa
Juan Pablo Canepa <jpcanepa@gmail.com>
2011-04-18 23:47:15 -0400

    Reformatted util.js

--- js/util.js
+++ js/util.js
@@ -1,37 +1,37 @@
-var Util = 
+var Util =
 {
-transformToCartesian : function(context)
-											 {
-												 context.setTransform(1, 0, 0, -1, 0, context.canvas.height);
-											 },
+    transformToCartesian: function(context)
+    {
+        context.setTransform(1, 0, 0, -1, 0, context.canvas.height);
+    },
 
-drawLine : function(context, x1, y1, x2, y2)
-					 {
-						 context.beginPath();
-						 context.moveTo(x1,y1);
-						 context.lineTo(x2,y2);
-						 context.closePath();
-						 context.stroke();
-					 },
+    drawLine: function(context, x1, y1, x2, y2)
+    {
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.closePath();
+        context.stroke();
+    },
 
-drawCircle : function(context, x, y, radius)
-						 {
-							 context.beginPath();
-							 context.arc(x, y, radius, 0, 2*Math.PI);
-							 context.closePath();
-							 context.stroke();
-						 },
+    drawCircle: function(context, x, y, radius)
+    {
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI);
+        context.closePath();
+        context.stroke();
+    },
 
-fillCircle : function(context, x, y, radius, fillColor)
-						 {
-							 context.save();
-							 context.beginPath();
-							 context.arc(x, y, radius, 0, 2*Math.PI);
-							 context.closePath();
-							 context.fillStyle = fillColor;
-							 context.fill();
-							 context.restore();			
-						 }
+    fillCircle: function(context, x, y, radius, fillColor)
+    {
+        context.save();
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI);
+        context.closePath();
+        context.fillStyle = fillColor;
+        context.fill();
+        context.restore();
+    }
 }
 /*
 	function Trace(destination) 
